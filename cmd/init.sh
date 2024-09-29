6e8e1c4677388fd3a4c6c167a971247305d73d36
xiabee <yunjie.xiao@pingcap.com.cn>
2024-09-29 11:27:54 +0800

    security test for v8.2.0

--- cmd/init.sh
+++ cmd/init.sh
@@ -94,10 +94,16 @@ sed -i 's/Test-Cluster-TiFlash-Proxy-Details/'$TIDB_CLUSTER_NAME'-TiFlash-Proxy-
 # TiCDC dashboard
 cp /tmp/ticdc.json $GF_PROVISIONING_PATH/dashboards
 sed -i 's/Test-Cluster-TiCDC/'$TIDB_CLUSTER_NAME'-TiCDC/g' $GF_PROVISIONING_PATH/dashboards/ticdc.json
+cp /tmp/TiCDC-Monitor-Summary.json $GF_PROVISIONING_PATH/dashboards
+sed -i 's/Test-Cluster-TiCDC-Summary/'$TIDB_CLUSTER_NAME'-TiCDC/g' $GF_PROVISIONING_PATH/dashboards/TiCDC-Monitor-Summary.json
 
 # TiKV-CDC dashboard
 cp /tmp/tikv-cdc.json $GF_PROVISIONING_PATH/dashboards
 sed -i 's/Test-Cluster-TiKV-CDC/'$TIDB_CLUSTER_NAME'-TiKV-CDC/g' $GF_PROVISIONING_PATH/dashboards/tikv-cdc.json
+
+# TiProxy dashboard
+cp /tmp/tiproxy_summary.json $GF_PROVISIONING_PATH/dashboards
+sed -i 's/Test-Cluster-TiProxy-Summary/'$TIDB_CLUSTER_NAME'-TiProxy-Summary/g' $GF_PROVISIONING_PATH/dashboards/tiproxy_summary.json
 
 # To support monitoring multiple clusters with one TidbMonitor, change the job label to component
 sed -i 's%job=\\\"tiflash\\\"%component=\\"tiflash\\"%g' $GF_PROVISIONING_PATH/dashboards/*.json
